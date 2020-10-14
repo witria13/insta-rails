7dc9caa4afb20059b897626777c1f2bda434e590
Witria13 <witriadsra@gmail.com>
2020-10-14 10:16:26 +0700

    menambahkan action like unlike

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,17 @@
 class LikesController < ApplicationController
    before_action :find_post
+
   def create
     @post.likes.create(user_id: User.first.id)
-    redirect_to post_path(@post)
+    redirect_to posts_path
   end
+
+
+  def destroy
+     @post.likes.find(params[:id]).destroy
+      redirect_to posts_path
+  end
+
   private
   def find_post
     @post = Post.find(params[:post_id])
